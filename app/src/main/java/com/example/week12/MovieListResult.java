db7fd30f36a74eb4d1d65645595bdca6eea6e434
unknown <99qkrtjdus@naver.com>
2022-05-24 15:43:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/week12/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.week12;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
